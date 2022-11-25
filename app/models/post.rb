56031ab89bae1339d042ca63b1a12538b00c24ad
Christabel <christabel.masakhwe@student.moringaschool.com>
2022-11-25 13:55:22 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+validates :title, presence: true
+validates :content, length: { minimum: 250 }
+validates :summary, length: { maximum: 250 }
+validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+ validate :title_is_clickbait
+    def title_is_clickbait
+        # unless title&.include?("Won't believe" || "Secret" || "Top [number]" ||"Guess" )
+        #     errors.add(:title, "We do not allow this title!")
+        # end
+        if title == "True Facts"
+            errors.add(:title, "Invalid")
+        end
+    end
 end
+
